1b610c94da34d67d473d43ef148bb1d497790ac9
Joe Pietruch <jcpigm@rit.edu>
2024-02-07 01:18:06 -0500

    Audio-06 Example

--- src/Dragger.ts
+++ src/Dragger.ts
@@ -4,64 +4,101 @@ export class Dragger {
   // Display elements
   element: HTMLElement
   labelElement: HTMLParagraphElement;
+  // more display elements
+  lower: HTMLDivElement;
+  upper: HTMLDivElement;
+  lbrow: HTMLDivElement;
+  rbrow: HTMLDivElement;
 
-  // Tracking the position
   coordinates: Coordinates
 
-  // Bookkeeping values used during dragging
   dragging: boolean = false
   offset: {x: number, y: number} = {x: 0, y: 0}
 
-  // Stores the callback for position updates
   positionCallback: ((x: Coordinates) => void) | null = null
 
   constructor(element: HTMLElement, coordinates: Coordinates) {
     this.element = element
     this.labelElement = document.createElement('p')
-    this.element.appendChild(this.labelElement)
 
-    // accept and apply the coordinates
+    // add a whole bunch of HTML nodes to make the character
+    // see here and in styles.css
+    this.lower = document.createElement('div')
+    this.lower.className = 'lower'
+    this.upper = document.createElement('div')
+    this.upper.className = 'upper'
+    this.element.appendChild(this.labelElement)
+    this.element.appendChild(this.lower)
+    this.element.appendChild(this.upper)
+    const eyes = document.createElement('div')
+    eyes.className = 'eyes'
+    this.lbrow = document.createElement('div')
+    this.lbrow.className = 'lbrow'
+    this.rbrow = document.createElement('div')
+    this.rbrow.className = 'rbrow'
+    this.upper.appendChild(eyes)
+    this.upper.appendChild(this.lbrow)
+    this.upper.appendChild(this.rbrow)
+
     this.coordinates = coordinates;
     this.applyCoordinates(coordinates.x, coordinates.y)
 
-    // when pressing down on a Dragger
     element.addEventListener('mousedown', (event) => {
       this.dragging = true
-      // the offset accounts for clicking on different parts of the Dragger
       this.offset.x = event.offsetX
       this.offset.y = event.offsetY
     })
 
-    // when the mouse moves in the window
     window.addEventListener('mousemove', (event) => {
-      // and this Dragger is dragging
       if(this.dragging){
-        // update its coordinates
         this.applyCoordinates(event.clientX - this.offset.x, event.clientY - this.offset.y)
       }
     })
 
-    // when releasing the mouse
     element.addEventListener('mouseup', (event) => {
-      // apply the coordinates
       this.applyCoordinates(event.clientX - this.offset.x, event.clientY - this.offset.y)
-      // and stop dragging
       this.dragging = false
     })
   }
 
-  // A label so we know which dot is which
   updateLabel(label: string) {
     this.labelElement.innerText = label
   }
 
-  // Provides a callback to be called whenever the coordinates update
+  // ordinarily I don't condone scattering class fields throughout the file
+  // but these are only used in sing(), so ... meh
+  rotation = 0
+  lastPercent = 0
+  sing(percent: number) : void {
+    if (Math.abs(percent - this.lastPercent) > 0.05) {
+      // when the percent changes by a lot in one frame, pick a random rotation
+      this.rotation = Math.random()*30 - 15;
+    }
+    if (this.lastPercent === 0 && percent > 0) {
+      // when starting to sing from zero, pick a random rotation
+      this.rotation = Math.random()*30 - 15;
+    }
+    if (percent === 0) {
+      // if it's silent, set the rotation to 0
+      this.rotation = 0
+    }
+    // change the eyebrow position when the rotation changes
+    const brow = Math.abs(this.rotation/15)
+    this.lbrow.style.translate = `0 ${brow*5}px`
+    this.rbrow.style.translate = `0 ${brow*3}px`
+
+    // rotate and translate the singer's head
+    this.upper.style.transform = `rotate(${this.rotation}deg) translateY(${-15 - percent*20}px)`;
+    
+    // keep track of the last value for next frame
+    this.lastPercent = percent
+  }
+
   registerUpdate(callback: (x: Coordinates) => void): void {
     this.positionCallback = callback
     this.positionCallback(this.coordinates)
   }
 
-  // Writes x and y into coordinates, the DOM node, and calls the update callback
   applyCoordinates(x: number, y: number): void {
     this.coordinates.x = x;
     this.coordinates.y = y;
--- src/Part.ts
+++ src/Part.ts
@@ -12,7 +12,9 @@ export class Part {
   sourceNode: MediaElementAudioSourceNode;
 
   gainNode: GainNode;
-  pannerNode: PannerNode; // oh look, a new type of Node
+  pannerNode: PannerNode;
+  analyzer: AnalyserNode;
+  analyzerData: Uint8Array;
 
   markup: HTMLDivElement;
   vocalRange: string;
@@ -28,42 +30,47 @@ export class Part {
     this.gainNode = audioContext.createGain()
     this.gainNode.gain.value = 1
 
-    this.dragger = dragger; // connect the dragger
-    this.dragger.updateLabel(vocalRange) // tell it which part it's associated with
+    // similar to audio-04b
+    this.analyzer = audioContext.createAnalyser()
+    this.analyzer.fftSize = 32 // smaller number of total samples, because we only really need one
+    this.analyzerData = new Uint8Array(this.analyzer.frequencyBinCount)
 
-    // So much configuration in the Panner...
+    this.dragger = dragger;
+    this.dragger.updateLabel(vocalRange)
+
     this.pannerNode = new PannerNode(audioContext, {
-      panningModel: 'HRTF', // "Head-Related Transfer Function" - mathematical model of audio influence of the human head
-      distanceModel: 'linear', // fall-off / fade-out function
+      panningModel: 'HRTF',
+      distanceModel: 'linear',
 
-      positionX: dragger.coordinates.x, // Where is the source at?
-      positionY: dragger.coordinates.y, // this will be updated as we drag things around
-      positionZ: dragger.coordinates.z, // we're going to mostly ignore z
+      positionX: dragger.coordinates.x,
+      positionY: dragger.coordinates.y,
+      positionZ: dragger.coordinates.z,
 
-      orientationX: 0, // What direction is the source pointing?
-      orientationY: 1, // (as a unit vector)
-      orientationZ: 0, // directionality is important
+      orientationX: 0,
+      orientationY: 1,
+      orientationZ: 0,
 
-      refDistance: 1, // scales the world units if necessary
-      maxDistance: 10000, // sets the furthest distance away something is still audible
-      rolloffFactor: 1, // how quickly does the volume reduce as moving away
+      refDistance: 1,
+      maxDistance: 10000,
+      rolloffFactor: 1,
 
-      coneInnerAngle: 120, // See this diagram : https://www.w3.org/TR/webaudio/#Spatialization-sound-cones
-      coneOuterAngle: 180 ,
-      coneOuterGain: 0.2,
+      coneInnerAngle: 120,
+      coneOuterAngle: 180,
+      coneOuterGain: 0.6,
     })
 
-    // Give the dragger a callback to update the panner position on drags
     this.dragger.registerUpdate(this.onNewCoordinates)
 
     this.vocalRange = vocalRange;
     this.markup = document.createElement('div');
 
     this.expandMarkup();
+
+    // setup our animation
+    window.requestAnimationFrame(this.measureAudio)
   }
 
   onNewCoordinates = (coordinates: Coordinates): void => {
-    // update the pannerNode position with the coordinates of the dragger
     this.pannerNode.positionX.value = coordinates.x;
     this.pannerNode.positionY.value = coordinates.y;
     this.pannerNode.positionZ.value = coordinates.z;
@@ -97,10 +104,19 @@ export class Part {
     this.markup.appendChild(h2)
     this.markup.appendChild(div)
   }
+  
+  measureAudio = (): void => {
+    // get the data for this frame
+    this.analyzer.getByteFrequencyData(this.analyzerData)
+    // send it to the dragger (I chose sample [8] because it made interesting animations)
+    this.dragger.sing(this.analyzerData[8]/255)
+    // ask for the next frame to keep going
+    window.requestAnimationFrame(this.measureAudio)
+  }
 
   patch(): AudioNode {
     // It's just another effect on the chain.
-    return this.sourceNode.connect(this.gainNode).connect(this.pannerNode);
+    return this.sourceNode.connect(this.gainNode).connect(this.analyzer).connect(this.pannerNode);
   }
   
   play(): void {
